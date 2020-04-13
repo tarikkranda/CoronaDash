806b2ad0d33d4e25afac639666f42c561e64479a
tarikkranda@yahoo.com <tarikkranda@yahoo.com>
2020-04-13 03:29:21 +0300

    Initial app boilerplate

---
+++ app.py
@@ -0,0 +1,173 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output
+import pandas as pd
+from datetime import datetime
+from os.path import isfile
+
+baseURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+fileNamePickle = "allData.pkl"
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+tickFont = {'size':12, 'color':"rgb(30,30,30)", 'family':"Courier New, monospace"}
+
+def loadData(fileName, columnName): 
+    data = pd.read_csv(baseURL + fileName) \
+             .drop(['Lat', 'Long'], axis=1) \
+             .melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name=columnName) \
+             .astype({'date':'datetime64[ns]', columnName:'Int64'}, errors='ignore')
+    data['Province/State'].fillna('<all>', inplace=True)
+    data[columnName].fillna(0, inplace=True)
+    return data
+
+def refreshData():
+    allData = loadData("time_series_covid19_confirmed_global.csv", "CumConfirmed") \
+        .merge(loadData("time_series_covid19_deaths_global.csv", "CumDeaths")) \
+        .merge(loadData("time_series_covid19_recovered_global.csv", "CumRecovered"))
+    allData.to_pickle(fileNamePickle)
+    return allData
+
+def allData():
+    if not isfile(fileNamePickle):
+        refreshData()
+    allData = pd.read_pickle(fileNamePickle)
+    return allData
+
+countries = allData()['Country/Region'].unique()
+countries.sort()
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+## App title, keywords and tracking tag (optional).
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-161733256-2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-161733256-2');
+        </script>
+        <meta name="keywords" content="COVID-19,Coronavirus,Dash,Python,Dashboard,Cases,Statistics">
+        <title>COVID-19 Case History</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+       </footer>
+    </body>
+</html>"""
+
+app.layout = html.Div(
+    style={ 'font-family':"Courier New, monospace" },
+    children=[
+        html.H1('Life Long Friends COVID-19 Dashboard'),
+        html.Div(className="row", children=[
+            html.Div(className="four columns", children=[
+                html.H5('Country'),
+                dcc.Dropdown(
+                    id='country',
+                    options=[{'label':c, 'value':c} for c in countries],
+                    value='Italy'
+                )
+            ]),
+            html.Div(className="four columns", children=[
+                html.H5('State / Province'),
+                dcc.Dropdown(
+                    id='state'
+                )
+            ]),
+            html.Div(className="four columns", children=[
+                html.H5('Selected Metrics'),
+                dcc.Checklist(
+                    id='metrics',
+                    options=[{'label':m, 'value':m} for m in ['Confirmed', 'Deaths', 'Recovered']],
+                    value=['Confirmed', 'Deaths']
+                )
+            ])
+        ]),
+        dcc.Graph(
+            id="plot_new_metrics",
+            config={ 'displayModeBar': False }
+        ),
+        dcc.Graph(
+            id="plot_cum_metrics",
+            config={ 'displayModeBar': False }
+        ),
+        dcc.Interval(
+            id='interval-component',
+            interval=3600*1000, # Refresh data each hour.
+            n_intervals=0
+        )
+    ]
+)
+
+@app.callback(
+    [Output('state', 'options'), Output('state', 'value')],
+    [Input('country', 'value')]
+)
+def update_states(country):
+    d = allData()
+    states = list(d.loc[d['Country/Region'] == country]['Province/State'].unique())
+    states.insert(0, '<all>')
+    states.sort()
+    state_options = [{'label':s, 'value':s} for s in states]
+    state_value = state_options[0]['value']
+    return state_options, state_value
+
+def filtered_data(country, state):
+    d = allData()
+    data = d.loc[d['Country/Region'] == country].drop('Country/Region', axis=1)
+    if state == '<all>':
+        data = data.drop('Province/State', axis=1).groupby("date").sum().reset_index()
+    else:
+        data = data.loc[data['Province/State'] == state]
+    newCases = data.select_dtypes(include='Int64').diff().fillna(0)
+    newCases.columns = [column.replace('Cum', 'New') for column in newCases.columns]
+    data = data.join(newCases)
+    data['dateStr'] = data['date'].dt.strftime('%b %d, %Y')
+    return data
+
+def barchart(data, metrics, prefix="", yaxisTitle=""):
+    figure = go.Figure(data=[
+        go.Bar( 
+            name=metric, x=data.date, y=data[prefix + metric],
+            marker_line_color='rgb(0,0,0)', marker_line_width=1,
+            marker_color={ 'Deaths':'rgb(200,30,30)', 'Recovered':'rgb(30,200,30)', 'Confirmed':'rgb(100,140,240)'}[metric]
+        ) for metric in metrics
+    ])
+    figure.update_layout( 
+              barmode='group', legend=dict(x=.05, y=0.95, font={'size':15}, bgcolor='rgba(240,240,240,0.5)'), 
+              plot_bgcolor='#FFFFFF', font=tickFont) \
+          .update_xaxes( 
+              title="", tickangle=-90, type='category', showgrid=True, gridcolor='#DDDDDD', 
+              tickfont=tickFont, ticktext=data.dateStr, tickvals=data.date) \
+          .update_yaxes(
+              title=yaxisTitle, showgrid=True, gridcolor='#DDDDDD')
+    return figure
+
+@app.callback(
+    [Output('plot_new_metrics', 'figure'), Output('plot_cum_metrics', 'figure')], 
+    [Input('country', 'value'), Input('state', 'value'), Input('metrics', 'value'), Input('interval-component', 'n_intervals')]
+)
+def update_plots(country, state, metrics, n):
+    refreshData()
+    data = filtered_data(country, state)
+    barchart_new = barchart(data, metrics, prefix="New", yaxisTitle="New Cases per Day")
+    barchart_cum = barchart(data, metrics, prefix="Cum", yaxisTitle="Cumulated Cases")
+    return barchart_new, barchart_cum
+
+server = app.server
+
+if __name__ == '__main__':
+    app.run_server(host="127.0.0.1")
